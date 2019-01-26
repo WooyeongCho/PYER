70d9297084c3d3a4108d41fa282af57ed539be40
WooyeongCho <nyangiwy24@naver.com>
2019-01-26 19:28:37 +0900

    fixed typing errors #2

--- water.js
+++ water.js
@@ -2,7 +2,7 @@ function addElement () {
   let elementExists = document.getElementById("water_pyer");
   if(!elementExists) {
     let random = Math.floor((Math.random() * 4));
-    let list = ["Pyer is the fire!", "Pyre!", "I feel Pyre!", "Fire!"];
+    let list = ["Pyer is the fire!", "Pyer!", "I feel Pyer!", "Fire!"];
     let para = document.createElement("P");
     let t = document.createTextNode(list[random]);
     para.style.cssText = 'color: white; position: absolute; top: 10px; left: 320px; font-weight:bold;-webkit-user-drag: none;-webkit-user-select: none;';
